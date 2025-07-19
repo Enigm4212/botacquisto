bebf1dd65e88ae8d2003f9079b9c07129a173b15
Enigm4212 <amado212@protonmail.com>
2025-07-19 14:07:16 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "7650029455:AAFkIN-RLPjL9hPbjPfGJ6em6N6gGtT7sAw"
+SUPERVISOR_CHAT_ID = 2066985588  # numero intero, senza virgolette
+WEBHOOK_URL = "https://il-tuo-nome-app.onrender.com/webhook"  # cambia dopo il deploy
